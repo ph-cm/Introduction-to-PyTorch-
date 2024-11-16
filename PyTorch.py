438b63deeb80074fe841ab4a9b73a2ab98a59828
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-16 17:42:57 -0300

    Computing gradients

--- PyTorch.py
+++ PyTorch.py
@@ -23,3 +23,25 @@ for i in a:
 print(s)
 
 torch.sum(a,axis=0)
+
+#Computing Gradients
+a = torch.randn(size = (2,2), requires_grad=True)
+b = torch.randn(size=(2,2))
+
+c = torch.mean(torch.sqrt(torch.square(a) + torch.square(b)))
+c.backward() #compute all gradients
+print(a.grad) #the gradient of 'c' with respect to 'a'
+
+c = torch.mean(torch.sqrt(torch.square(a) + torch.square(b)))
+c.backward(retain_graph=True)
+c.backward(retain_graph=True)
+print(a.grad)
+a.grad.zero_()
+c.backward()
+print(a.grad)
+
+print(c)
+
+c = torch.sqrt(torch.square(a) + torch.square(b))
+c.backward(torch.eye(2)) #eye == 2x2 identity matrix
+print(a.grad)
