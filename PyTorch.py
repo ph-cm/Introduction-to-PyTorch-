559d1a14d1022ca6b826df5fcace3776ddc5b616
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-16 19:46:06 -0300

    Linear Regression

--- PyTorch.py
+++ PyTorch.py
@@ -1,5 +1,9 @@
 import torch
-torch.__version__
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification, make_regression
+from sklearn.model_selection import train_test_split
+import random
 
 #Simple Tensors
 a = torch.tensor([[1,2],[3,4]])
@@ -58,4 +62,58 @@ for i in range(15):
     x.data.add_(-lr*gr)
     x.grad.zero_()
     print("Step {}: x[0]={}, x[1]={}".format(i,x[0],x[1]))
+    
+#Example: Linear Regression
+
+np.random.seed(13)
+train_x = np.linspace(0, 3, 120)
+train_labels = 2 * train_x + 0.9 + np.random.randn(*train_x.shape) * 0.5
+
+plt.scatter(train_x, train_labels)
+plt.show()
+
+input_dim = 1
+output_dim = 1
+learning_rate = 0.1
+
+#This is our weight matrix
+w = torch.tensor([100.0], requires_grad=True, dtype=torch.float32)
+# This is our Bias vector
+b = torch.zeros(size=(output_dim,), requires_grad=True)
+
+def f(x):
+    return torch.matmul(x,w) + b
+
+def compute_loss(labels, predictions):
+    return torch.mean(torch.square(labels - predictions))
+
+def train_on_batch(x,y):
+    predictions = f(x)
+    loss = compute_loss(y, predictions)
+    loss.backward()
+    w.data.sub_(learning_rate * w.grad)
+    b.data.sub_(learning_rate * b.grad)
+    w.grad.zero_()
+    b.grad.zero_()
+    return loss
+
+#shuffe data
+indices = np.random.permutation(len(train_x))
+features = torch.tensor(train_x[indices], dtype=torch.float32)
+labels = torch.tensor(train_labels[indices],dtype=torch.float32)
+
+batch_size = 4
+for epoch in range(10):
+    for i in range(0,len(features), batch_size):
+        loss = train_on_batch(features[i:i+batch_size].view(-1,1), labels[i:i+batch_size])
+    print('Epoch %d: last batch loss = %.4f' % (epoch, float(loss)))
+
+w,b
+
+plt.scatter(train_x, train_labels)
+x = np.array([min(train_x), max(train_x)])
+with torch.no_grad():
+    y = w.numpy()*x+b.numpy()
+plt.plot(x,y,color='red')
+plt.show()
     
