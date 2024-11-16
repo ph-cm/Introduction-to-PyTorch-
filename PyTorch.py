3b1f1a1fd2076bcae1ce62cc46b8b9b9bed16089
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-16 17:36:48 -0300

    In-place and out-place operations

--- PyTorch.py
+++ PyTorch.py
@@ -1,7 +1,25 @@
 import torch
 torch.__version__
 
+#Simple Tensors
 a = torch.tensor([[1,2],[3,4]])
 print(a)
 a = torch.randn(size=(10,3))
 print(a)
+
+print(a-a[0])
+print(torch.exp(a)[0].numpy())
+
+#In-place and ou-place operations
+u = torch.tensor(5)
+print("Result when adding out-of-place: ", u.add(torch.tensor(3)))
+u.add_(torch.tensor(3))
+print("Result after adding in-place: ", u)
+
+s = torch.zeros_like(a[0])
+for i in a:
+    s.add_(i)
+    
+print(s)
+
+torch.sum(a,axis=0)
